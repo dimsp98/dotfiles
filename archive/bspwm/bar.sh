0c9412fa5dfe39ed3981a5e142470e6d9f297d96
dimsp98 <d.spiliotis@tutanota.com>
2025-07-16 23:18:19 +0300

    archived old config files

---
+++ archive/bspwm/bar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+pkill polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c $HOME/.config/polybar/config.ini &
+  done
+else
+  polybar -c $HOME/.config/polybar/config.ini &
+fi
+
+echo "Polybar launched"
