6f6d97878cdceea8b1aec885484c34659dae41ec
dimsp98 <d.spiliotis@tutanota.com>
2025-07-17 17:18:23 +0300

    updated setup scripts to integrate stow

--- setup-scripts/arch-hyprland.sh
+++ setup-scripts/arch-hyprland.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 #Install packages
-sudo pacman -S --needed kitty hyprland hyprlock hyprshot hyprpaper waybar thunar thunar-archive-plugin thunar-volman xarchiver wofi wlogout nwg-look papirus-icon-theme
+sudo pacman -S --needed stow kitty hyprland hyprlock hyprshot hyprpaper waybar thunar thunar-archive-plugin thunar-volman xarchiver wofi wlogout nwg-look papirus-icon-theme
 
 #Symlink config files
-ln -sf $HOME/dotfiles/config/hypr/ $HOME/.config
-ln -sf $HOME/dotfiles/config/waybar/ $HOME/.config
-ln -sf $HOME/dotfiles/config/wofi/ $HOME/.config
-ln -sf $HOME/dotfiles/config/wlogout/ $HOME/.config
-sudo ln -sf $HOME/dotfiles/config/wlogout/icons/* /usr/share/wlogout/icons/
-sudo ln -sf $HOME/dotfiles/themes/Catppuccin-Mocha-Standard-Sapphire-Dark/ /usr/share/themes/
-sudo ln -sf $HOME/dotfiles/icons/catppuccin-mocha-sapphire-cursors/ /usr/share/icons/
+cd dotfiles
+stow -t $HOME/.config .config/
--- setup-scripts/void.sh
+++ setup-scripts/void.sh
@@ -1,43 +1,57 @@
 #!/bin/sh
-#Install Dependencies
-cd ~ && echo "Installing Dependencies"
-sudo xbps-install arandr acpi rustup pnpm kitty elogind rofi picom void-repo-nonfree xarchiver thunar-media-tags-plugin thunar-archive-plugin 
+# Install Dependencies
+echo "Installing Dependencies"
+sudo xbps-install stow arandr acpi rustup pnpm kitty elogind rofi picom void-repo-nonfree xarchiver thunar-media-tags-plugin thunar-archive-plugin 
+
 sudo xbps-install void-repo-multilib void-repo-multilib-nonfree nodejs pnpm xorg base-devel git bat papirus-icon-theme papirus-folders lf ffmpeg ghostscript zip unzip gzip 
+
 sudo xbps-install lxappearance libX11-devel libXinerama-devel libXft-devel xsetroot curl openssh wget bspwm htop pipewire pamixer pavucontrol ncmpcpp sxhkd xclip dunst vlc maim
+
 sudo xbps-install zig fish neovim polybar jsoncpp feh zathura-pdf-mupdf firefox
+
 sudo xbps-remove -R gnome-keyring
-#creating directories
+
+# Choose folder color
 echo "Setting folder Theme"
 papirus-folders -C cyan --theme Papirus-Dark
-#Install and apply my dotfiles
+
+# Install and apply my dotfiles
 cd ~/dotfiles && echo "Creating symlinks"
 git submodule update --init
-ln -sf $HOME/dotfiles/config/* $HOME/.config/
-ln -sf $HOME/dotfiles/.zshenv ~
-sudo ln -sf $HOME/dotfiles/fonts/* /usr/share/fonts/
-sudo ln -sf $HOME/dotfiles/icons/* /usr/share/icons/
-sudo ln -sf $HOME/dotfiles/themes/* /usr/share/themes/
-#void-packages
+
+# Create symlinks using stow
+stow -t $HOME/.config config
+stow -t /usr/share/ fonts
+stow -t /usr/share/ icons
+stow -t /usr/share/ themes
+
+# Setup void-packages
 echo -e "Installing void-packages"
 cd ~ && git clone https://github.com/void-linux/void-packages.git
 cd void-packages
 ./xbps-src binary-bootstrap
 echo XBPS_ALLOW_RESTRICTED=yes >>etc/conf
-#Change theme for bat
+
+# Change theme for bat
 bat cache --build
-#Change shell to zsh
+
+# Change shell to zsh
 chsh -s /usr/bin/zsh && sudo chsh -s /usr/bin/zsh
-#NVim
+
+# NVim
 rustup toolchain install nightly
 sudo xbps-install tree-sitter zoxide lazygit ripgrep sqlite fd yarn lldb nvm gcc
 cd ~
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
-#Configure fonts
+
+# Configure fonts
 sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 sudo xbps-reconfigure -f fontconfig
-#Dual-boot Timezone
+
+# Dual-boot Timezone
 echo "HARDWARECLOCK="localtime"" | sudo tee /etc/rc.conf
 cd ~
-#Setting up services
+
+# Setting up services
 echo -e "Setting up servicces"
 sudo ln -s /etc/sv/dbus /var/service
