635bd5af458c00eef33122e97d132d7f5ba69eb8
dimsp98 <d.spiliotis@tutanota.com>
2025-07-17 23:07:22 +0300

    updated

--- dot_config/waybar/scripts/keyboard-layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-layout=$(hyprctl devices -j | jq -r '
-  .keyboards[] | select(.name != null) | .active_keymap' | head -n1)
-case "$layout" in
-  us) echo "US" ;;
-  gr) echo "GR" ;;
-  *) echo "$layout" ;;
-esac
