30f44703a61ce046a5597f7e2c1fc91169636f6c
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2025-07-18 07:22:21 +0000

    Update arch-hyprland.sh

--- setup-scripts/arch-hyprland.sh
+++ setup-scripts/arch-hyprland.sh
@@ -3,6 +3,7 @@
 sudo pacman -S --needed stow kitty hyprland hyprlock hyprshot hyprpaper waybar thunar thunar-archive-plugin thunar-volman xarchiver wofi wlogout nwg-look papirus-icon-theme
 
 #Symlink config files
+git clone https://github.com/dimsp98/dotfiles.git
 cd dotfiles
 stow -t $HOME/.config config
 sudo stow -t /usr/share share
