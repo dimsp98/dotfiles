297c907a0ffef548c1755917572e01735fa0db47
dimsp98 <d.spiliotis@tutanota.com>
2025-07-24 19:52:41 +0300

    updated clipboard manager and rofi

--- setup-scripts/arch-hyprland.sh
+++ setup-scripts/arch-hyprland.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Install packages
-sudo pacman -S --needed chezmoi kitty hyprland hyprlock hyprshot hyprpaper waybar nvim lf thunar thunar-archive-plugin fish thunar-volman gvfs gvfs-smb gvfs-mtp xarchiver rofi-wayland wlogout dunst nwg-look papirus-icon-theme cliphist uwsm mpd rmpc xcb-imdkit meson
+sudo pacman -S --needed chezmoi kitty hyprland hyprlock hyprshot hyprpaper waybar nvim lf thunar thunar-archive-plugin fish thunar-volman gvfs gvfs-smb gvfs-mtp xarchiver rofi-wayland wlogout dunst nwg-look papirus-icon-theme cliphist uwsm mpd rmpc xcb-imdkit meson hyprpolkitagent
 
 #Symlink config files
 chezmoi init https://github.com/dimsp98/dotfiles.git
