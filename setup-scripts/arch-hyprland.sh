d4872a38982b2d9b47e5090462c085594f8daba4
dimsp98 <d.spiliotis@tutanota.com>
2025-07-28 19:58:57 +0300

    updated setup script

--- setup-scripts/arch-hyprland.sh
+++ setup-scripts/arch-hyprland.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# Update system
+sudo pacman -Syu --noconfirm
+
 #Install packages
-sudo pacman -S --needed chezmoi kitty hyprland hyprlock hyprshot hyprpaper waybar nvim lf thunar thunar-archive-plugin fish thunar-volman gvfs gvfs-smb gvfs-mtp xarchiver rofi wlogout dunst nwg-look papirus-icon-theme cliphist uwsm mpd rmpc xcb-imdkit meson hyprpolkitagent
+sudo pacman -S --noconfirm --needed chezmoi kitty hyprland hyprlock hyprshot hyprpaper waybar nvim lf thunar thunar-archive-plugin fish thunar-volman gvfs gvfs-smb gvfs-mtp xarchiver rofi wlogout dunst nwg-look papirus-icon-theme cliphist uwsm mpd rmpc xcb-imdkit meson hyprpolkitagent
 
 #Symlink config files
 chezmoi init https://github.com/dimsp98/dotfiles.git
