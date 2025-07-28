c39bf01b301263f967c16d7dd93abbb2b256d830
dimsp98 <d.spiliotis@tutanota.com>
2025-07-28 19:47:54 +0300

    added hyprpaper reload script

---
+++ dot_config/hypr/executable_wallpaper.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q hyprpaper
+
+# Wait until the processes have been shut down
+while pgrep -x hyprpaper >/dev/null; do sleep 1; done
+
+# Launch main
+hyprpaper -c $HOME/.config/hypr/hyprpaper.conf
