7010a63a71f81d9d747b74fa91e877e663a6c773
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2025-08-05 14:00:52 +0300

    Update oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,13 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  "stevearc/oil.nvim",
+  dependencies = {
+    { "echasnovski/mini.icons", lazy = false },
+    { "nvim-tree/nvim-web-devicons" }
+  },
+  config = function()
+    local oil = require("oil")
+    oil.setup()
+    vim.keymap.set("n", "-", oil.toggle_float, {})
+  end,
   lazy = false,
 }
