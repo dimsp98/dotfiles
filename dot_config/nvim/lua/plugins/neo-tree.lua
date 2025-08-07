ad8979f65d9886ff3f6bef57a2d06637f44cfdfd
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2025-08-07 15:05:41 +0300

    Update neo-tree.lua

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,49 @@
-return   {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
   },
-  lazy = false, -- neo-tree will lazily load itself
-  opts = {
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
   },
-  config = function ()
-  require('neo-tree').setup {
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "2.*",
+    config = function()
+require('neo-tree').setup {
   filesystem = {
     filtered_items = {
       visible = true,
       hide_dotfiles = false,
       hide_gitignored = true,
-    }
-    }
-    }
-  vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
   end
+    },
+      require("window-picker").setup({
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { "terminal", "quickfix" },
+          },
+        },
+      })
+    end,
+  },
 }
-
