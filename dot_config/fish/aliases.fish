79356c1b207e92054b3560a0c2ebd723996a7a5c
dimsp98 <d.spiliotis@tutanota.com>
2025-08-17 11:58:52 +0300

    separated aliases from config.fish and removed sudope plugin

---
+++ dot_config/fish/aliases.fish
@@ -0,0 +1,69 @@
+## Aliases
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+
+#Replace find with fd 
+alias find='fd'
+
+# Common use
+alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
+alias update='sudo pacman -Syu'
+alias install='sudo pacman -S'
+alias remove='sudo pacman -Rns'
+
+# Git
+alias ga='git add'
+alias gp='git push'
+alias gc='git clone'
+
+# Chezmoi
+alias dot='chezmoi cd'
+alias cmap='chezmoi apply -v'
+alias cmdiff='chezmoi diff'
+alias cmup='chezmoi update -v'
+
+# Void Linux XBPS
+alias xi='sudo xbps-install'
+alias xr='sudo xbps-remove -R'
+alias xup='sudo xbps-install -Suv'
+
+# Compiling
+alias mk='sudo make install'
+
+# Neovim
+alias v='nvim'
+alias sv='sudo nvim'
+
+# Get fastest mirrors
+alias mirror="sudo cachyos-rate-mirrors"
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,6 +9,9 @@ starship init fish | source
 ## Source from conf.d before our fish config
 source $HOME/.config/fish/conf.d/done.fish
 
+# Source aliases
+source $HOME/.config/fish/aliases.fish
+
 # Format man pages
 set -x MANROFFOPT "-c"
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
@@ -43,76 +46,5 @@ else
   bind ! __history_previous_command
   bind '$' __history_previous_command_arguments
 end
-
-## Useful aliases
-# Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons'  # long format
-alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
-
-#Replace find with fd 
-alias find='fd'
-
-# Common use
-alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-alias tarnow='tar -acf '
-alias untar='tar -zxvf '
-alias wget='wget -c '
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias hw='hwinfo --short'                                   # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
-alias update='sudo pacman -Syu'
-alias install='sudo pacman -S'
-alias remove='sudo pacman -Rns'
-
-# Git
-alias ga='git add'
-alias gp='git push'
-alias gc='git clone'
-
-# Chezmoi
-alias dot='chezmoi cd'
-alias cmap='chezmoi apply -v'
-alias cmdiff='chezmoi diff'
-alias cmup='chezmoi update -v'
-
-# Void Linux XBPS
-alias xi='sudo xbps-install'
-alias xr='sudo xbps-remove -R'
-alias xup='sudo xbps-install -Suv'
-
-# Compiling
-alias mk='sudo make install'
-
-# Neovim
-alias v='nvim'
-alias sv='sudo nvim'
-
-# Get fastest mirrors
-alias mirror="sudo cachyos-rate-mirrors"
-
-# Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# Recent installed packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 zoxide init fish | source
