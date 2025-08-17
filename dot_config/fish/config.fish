3d5aa16364f4ac0b7fc435db22c0fd39cc8a8fa9
dimsp98 <d.spiliotis@tutanota.com>
2025-08-17 12:26:29 +0300

    config.fish cleanup

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -26,12 +26,6 @@ set -x STARSHIP_CONFIG $HOME/.config/starship.toml
 set -U __done_min_cmd_duration 10000
 set -U __done_notification_urgency_level low
 
-## Environment setup
-# Apply .profile: use this to put fish compatible .profile stuff in
-if test -f ~/.fish_profile
-  source ~/.fish_profile
-end
-
 # Add ~/.local/bin to PATH
 if test -d ~/.local/bin
     if not contains -- ~/.local/bin $PATH
