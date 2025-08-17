f07ae9fc79fd2137dc6fc5808b1c75066a23ce46
dimsp98 <d.spiliotis@tutanota.com>
2025-08-17 16:00:13 +0300

    modified fish config

---
+++ dot_config/fish/conf.d/environment.fish
@@ -0,0 +1,28 @@
+set -g fish_greeting
+
+# Default programs 
+set -Ux TERMINAL foot 
+set -gx EDITOR nvim 
+set -x STARSHIP_CONFIG $HOME/.config/starship.toml
+
+# Format man pages
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# Directories
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_RUNTIME_DIR /run/user/(id -u)
+
+# Set settings for https://github.com/franciscolourenco/done 
+set -U __done_min_cmd_duration 5000
+set -U __done_notification_urgency_level low
+set -U __done_notification_urgency_level_failure normal
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,25 +1,3 @@
-set -g fish_greeting
-
-# Format man pages
-set -x MANROFFOPT "-c"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-# Default programs
-set -x STARSHIP_CONFIG $HOME/.config/starship.toml
-
-# Directories
-set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CACHE_HOME $HOME/.cache
-set -gx XDG_RUNTIME_DIR /run/user/(id -u)
-
-# Add ~/.local/bin to PATH
-if test -d ~/.local/bin
-    if not contains -- ~/.local/bin $PATH
-        set -p PATH ~/.local/bin
-    end
-end
-
 ## Keybindings for history functions
 if [ "$fish_key_bindings" = fish_vi_key_bindings ];
   bind -Minsert ! __history_previous_command
