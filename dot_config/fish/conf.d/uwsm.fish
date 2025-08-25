f5a40228bc243a3469343189966f03ea495c4725
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2025-08-25 13:24:53 +0300

    Delete dot_config/fish/conf.d/uwsm.fish
    Signed-off-by: dimsp98 <125738354+dimsp98@users.noreply.github.com>

--- dot_config/fish/conf.d/uwsm.fish
+++
@@ -1,9 +0,0 @@
-## Start uwsm if allowed and not already running
-if status is-login
-    if command -q uwsm
-        if uwsm check may-start; and uwsm select
-            exec uwsm start default
-        end
-    end
-end
-
