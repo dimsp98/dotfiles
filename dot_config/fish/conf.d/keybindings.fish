78183ed058cbcad2b1b0e9fcc287652c98b9d119
dimsp98 <d.spiliotis@tutanota.com>
2026-01-03 09:51:37 +0200

    updated fish config

--- dot_config/fish/conf.d/keybindings.fish
+++
@@ -1,8 +0,0 @@
-## Keybindings for history functions
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
